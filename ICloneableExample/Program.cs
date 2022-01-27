761e9edcc04e4f0e40c59b1263aa41670351ce1b
GreedNeSS <ruslan199126@gmail.com>
2022-01-27 20:44:38 +0300

    Добавьте файлы проекта.

---
+++ ICloneableExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            OperatingSystem operatingSystem = new(PlatformID.Win32NT, new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn =
+              new System.Data.SqlClient.SqlConnection();
+
+            CloneMe(str);
+            CloneMe(operatingSystem);
+            CloneMe(sqlCnn);
+            Console.ReadLine();
+        }
+
+        private static void CloneMe(ICloneable c)
+        {
+            object theClone = c.Clone();
+            Console.WriteLine($"Your clone is a: {c.GetType().Name}");
+        }
+    }
+}
